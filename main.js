dad71afb22194e565dc65566b8c9954b170d6331
Ley official <danyilfreelance@gmail.com>
2023-07-31 23:58:09 +0100

    Finish SUPER FIX

--- main.js
+++ main.js
@@ -2,14 +2,42 @@ const form = document.querySelector('.main')
 const input = document.querySelector('.input');
 const taskList = document.querySelector('.whole')
 
-let tasks = [];
+const block = document.querySelector('.registration')
+const password = document.querySelector('#logpass');
+const login = document.querySelector('#logemail');
+const main = document.querySelector('.main-main');
 
+const btnOut = document.querySelector('.sign-in');
+const modal = document.querySelector('.modal-alert');
+const modalText = document.querySelector('.text-alert');
+const closeModalPrevent = document.querySelector('.close-modal');
+
+
+let tasks = [];
+window.onload = () => {
+  let forTimeout;
+  if (localStorage.getItem('login') === 'true') {
+    block.classList.add('hidden');
+    main.classList.remove('blur')
+    createNotify('Добро пожаловать!')
+  }
+}
+if (localStorage.getItem('login') === true) {
+  if (login.value === 'test' && password.value === '1234') {
+    block.classList.add('hidden');
+    main.classList.remove('blur')
+    localStorage.setItem('login', true);
+
+  }
+}
 if (localStorage.getItem('tasks')) {
   tasks = JSON.parse(localStorage.getItem('tasks'));
   tasks.forEach(task => {
     renderTask(task)
   });
 }
+
+btnOut.addEventListener('click', signout)
 
 form.addEventListener('submit', addTask);
 
@@ -21,7 +49,10 @@ function addTask(event) {
   event.preventDefault();
 
   const inputValue = input.value;
+  if (inputValue.trim().length === 0) {
+    return createNotify('Поле ввода не должно быть пустым!')
 
+  }
   const info = {
     id: Date.now(),
     text: inputValue,
@@ -46,7 +77,8 @@ function doneTask(event) {
   task.done = !task.done
 
   const taskTitle = parentNode.querySelector('.dolist-text');
-  taskTitle.classList.toggle('dolist-text--cross')
+
+  taskTitle.classList.toggle('dolist-text--cross');
 
   saveToLS()
 }
@@ -83,11 +115,50 @@ function renderTask(task) {
     </div>
   </div>
   <div class="input-answer">
-    <p class="${cssStyle}" >
+    <p class="dolist-text ${cssStyle}" >
     ${task.text}
     </p>
   </div>
 </div>
 `
   taskList.insertAdjacentHTML('beforeend', taskHTML);
+}
+
+
+document.querySelector('.btn').addEventListener('click', loginE);
+
+function loginE(e) {
+  e.preventDefault()
+  if (login.value === 'test' && password.value === '1234') {
+    block.classList.add('hidden');
+    main.classList.remove('blur')
+    localStorage.setItem('login', true);
+    createNotify('Добро пожаловать!')
+  }
+}
+
+function signout() {
+  localStorage.clear()
+  location.reload()
+};
+
+function createNotify(text) {
+
+  modal.classList.toggle('gonne');
+  modal.classList.toggle('active')
+  modalText.innerHTML = text
+
+  forTimeout = setTimeout(() => {
+    modal.classList.toggle('gonne');
+    modal.classList.toggle('active')
+  }, 4000)
+
+  closeModalPrevent.addEventListener('click', closeModalPermanent);
+}
+
+function closeModalPermanent() {
+  modal.classList.toggle('gonne');
+  modal.classList.toggle('active')
+  clearTimeout(forTimeout);
+
 }
